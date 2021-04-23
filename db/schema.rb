8894726a8dfbb6ddd0cb5d072e07050a89442770
Romain Vigo Benia <romain.vigobenia@gmail.com>
2021-04-23 14:27:43 +0200

    chore: setup rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,15 +12,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_120123) do
-
-  create_table "courses", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "thumbnail_url"
-    t.string "public_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_210_423_120_123) do
+  create_table 'courses', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.string 'thumbnail_url'
+    t.string 'public_url'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :course do
-    title { "My crash coures" }
-    description  { "Best course ever" }
+    title { 'My crash coures' }
+    description { 'Best course ever' }
     thumbnail_url {}
     public_url { 'https://www.google.com' }
   end
