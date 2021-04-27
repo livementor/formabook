258c4e38aa7f83349414f573a8f004d4a314fb1d
Edward Schults <edward.schults@gmail.com>
2021-04-27 12:55:06 +0200

    fix: add thumbnail fallback url

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@
 class Course < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: true
+
+  def thumbnail_url
+    read_attribute(:thumbnail_url) || 'https://www.livementor.com/wp-content/uploads/2019/04/new-logo.png'
+  end
 end
